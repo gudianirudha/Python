887a879c342740cc02a3d6a83fab288644eda142
Anirudha gudi <54411756+gudianirudha@users.noreply.github.com>
2020-11-11 14:48:57 +0530

    Create random.py

---
+++ random.py
@@ -0,0 +1,10 @@
+import random
+
+# A floating point number between 1 and 50
+num = random.uniform(1, 50)
+print(num)
+
+
+num=random.randint(0,40)
+#The randint function takes in a range and produces an integer between that range.
+print(num)
