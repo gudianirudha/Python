21e3a0570f2984bcaf5499ebf6557bf648f7e1c6
Anirudha gudi <54411756+gudianirudha@users.noreply.github.com>
2020-11-11 14:19:38 +0530

    zipfunction.py

--- zipfunction.py
+++ zipfunction.py
@@ -2,3 +2,12 @@ names=("Anirudha","Kumar","Suresh","Venugopal")
 branches=("ISE","CSE","MECH","EE")
 info=list(zip(names,branches))
 print(info)
+
+#2nd example
+names=("Anirudha","Kumar","Suresh","Venugopal","Sukesh")
+branches=("ISE","CSE","MECH","EE","EC")
+
+info=(zip(names,branches))
+for a,b in info:
+    print(a,b)
+  
