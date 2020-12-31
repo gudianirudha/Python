db3afbf004570514638414e990603f134650471e
Ani <50128731+AnikethSDeshpande@users.noreply.github.com>
2020-12-31 16:43:35 +0530

    Update generators.py
    this should help

--- generators.py
+++ generators.py
@@ -10,24 +10,24 @@ def inclusive_range(*args):
     
 
 
-#intialize the parameters
-if numargs<1:
-    raise TypeError(f'expected at least 1 arguement{numargs}')
-elif numargs == 1:
-    stop = args[0]
-elif numargs == 2:
-    (start,stop) = args
-elif numargs == 3:
-    (start,stop,step) = args
-else:
-    raise TypeError(f'Atleast 3 arguements{numargs} required')
+    #intialize the parameters
+    if numargs<1:
+        raise TypeError(f'expected at least 1 arguement{numargs}')
+    elif numargs == 1:
+        stop = args[0]
+    elif numargs == 2:
+        (start,stop) = args
+    elif numargs == 3:
+        (start,stop,step) = args
+    else:
+        raise TypeError(f'Atleast 3 arguements{numargs} required')
 
-    
-#generator
-i = start
-while i<=stop:
-    yield i
-    i = i + 1
+
+    #generator
+    i = start
+    while i<=stop:
+        yield i
+        i = i + 1
 
 if __name__== "__main__":
     main()
